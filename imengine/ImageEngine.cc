210198dd3f97248b92716f44d153e1e97ddf7500
David Kirkby <dkirkby@uci.edu>
2011-01-08 10:00:30 -0800

    Skeleton library class

---
+++ imengine/ImageEngine.cc
@@ -0,0 +1,11 @@
+// Created 8-Jan-2011 by David Kirkby <dkirkby@uci.edu>
+
+#include "imengine/ImageEngine.h"
+
+namespace local = imengine;
+
+local::ImageEngine::ImageEngine() {
+}
+
+local::ImageEngine::~ImageEngine() {
+}
---
+++ imengine/ImageEngine.h
@@ -0,0 +1,15 @@
+// Created 8-Jan-2011 by David Kirkby <dkirkby@uci.edu>
+
+#ifndef IMENGINE_IMAGE_ENGINE_H
+#define IMENGINE_IMAGE_ENGINE_H
+
+namespace imengine {
+    class ImageEngine {
+    public:
+        ImageEngine();
+        virtual ~ImageEngine();
+    private:
+    };
+} // imengine::
+
+#endif // IMENGINE_IMAGE_ENGINE_H
