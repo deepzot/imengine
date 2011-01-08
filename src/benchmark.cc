051bf48080ab531fb457e56e1ecd6c9cc26eddde
David Kirkby <dkirkby@uci.edu>
2011-01-08 10:09:12 -0800

    add skeleton bin source

---
+++ src/benchmark.cc
@@ -0,0 +1,11 @@
+// Created 8-Jan-2011 by David Kirkby <dkirkby@uci.edu>
+
+#include "imengine/ImageEngine.h"
+
+using namespace imengine;
+using namespace std;
+
+int main(int argc, char **argv) {
+    ImageEngine *ie = new ImageEngine();
+    delete ie;
+}
