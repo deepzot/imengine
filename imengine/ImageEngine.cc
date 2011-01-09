aa4ae7fa2a28905ea3f3145c1ff430608400f64a
David Kirkby <dkirkby@uci.edu>
2011-01-09 10:36:08 -0800

    flesh out engine interface

--- imengine/ImageEngine.cc
+++ imengine/ImageEngine.cc
@@ -1,9 +1,17 @@
 // Created 08-Jan-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "imengine/ImageEngine.h"
+#include "imengine/AbsPixelFunction.h"
 
 namespace local = imengine;
 
-local::ImageEngine::ImageEngine() { }
+local::ImageEngine::ImageEngine(const local::AbsPixelFunction& source, 
+const local::AbsPixelFunction& psf, int pixelsPerSide) {
+    
+}
 
 local::ImageEngine::~ImageEngine() { }
+
+local::ImageData *local::ImageEngine::generate(double pixelScale, double dx, double dy) {
+    return 0;
+}
--- imengine/ImageEngine.h
+++ imengine/ImageEngine.h
@@ -4,10 +4,17 @@
 #define IMENGINE_IMAGE_ENGINE
 
 namespace imengine {
+    class AbsPixelFunction;
+    class ImageData;
+    // Generates pixelized images of a source convoluted with a psf.
 	class ImageEngine {
 	public:
-		ImageEngine();
+	    // Creates a new engine for the specified source and psf functions to generate
+	    // square images of pixelsPerSide x pixelsPerSide pixels.
+		ImageEngine(const AbsPixelFunction &source, const AbsPixelFunction &psf, int pixelsPerSide);
 		virtual ~ImageEngine();
+		// Generates an image at the specified pixel scale, with the source function offset by (dx,dy)
+        ImageData *generate(double pixelScale = 1, double dx = 0, double dy = 0);
 	private:
 	}; // ImageEngine
 } // imengine
