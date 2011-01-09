dcb38e489613a664a0c7b5e3f2e60eef23ccdc1c
David Kirkby <dkirkby@uci.edu>
2011-01-09 10:23:21 -0800

    declare abstract interface

--- imengine/AbsPixelFunction.h
+++ imengine/AbsPixelFunction.h
@@ -4,10 +4,18 @@
 #define IMENGINE_ABS_PIXEL_FUNCTION
 
 namespace imengine {
+    // Declares the abstract interface for a real-valued function on a 2D pixel space.
 	class AbsPixelFunction {
 	public:
 		AbsPixelFunction();
 		virtual ~AbsPixelFunction();
+		// Returns the function value
+        virtual double operator()(double x, double y) const = 0;
+        // Fills the provided array with the function's discrete Fourier transform on
+        // a square ngrid x ngrid lattice with the specified spacing and (x,y) origin
+        // relative to the grid center.
+        virtual void fillArrayWithTransform(double **array, int ngrid,
+            double spacing = 1, double dx = 0, double dy = 0) const = 0;
 	private:
 	}; // AbsPixelFunction
 } // imengine
