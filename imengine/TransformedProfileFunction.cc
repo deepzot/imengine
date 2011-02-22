736ebd2ce60cc453581e5c96c6d69708d8ab05cb
David Kirkby <dkirkby@uci.edu>
2011-02-22 14:58:15 -0800

    flesh out dependencies on abs profile and coord transform

--- imengine/TransformedProfileFunction.cc
+++ imengine/TransformedProfileFunction.cc
@@ -1,17 +1,26 @@
 // Created 22-Feb-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "imengine/TransformedProfileFunction.h"
+#include "imengine/AbsRadialProfile.h"
+#include "imengine/AbsCoordTransform.h"
+
+#include "boost/bind.hpp"
 
 #include <cassert>
 
 namespace local = imengine;
 
-local::TransformedProfileFunction::TransformedProfileFunction() { }
+local::TransformedProfileFunction::TransformedProfileFunction(
+AbsRadialProfile const &profile, AbsCoordTransform &transform) :
+_radialProfile(profile), _coordTransform(transform)
+{        
+}
 
 local::TransformedProfileFunction::~TransformedProfileFunction() { }
 
 double local::TransformedProfileFunction::operator()(double x, double y) const {
-    return 0;
+    double r(_coordTransform.transformedRadius(x,y));
+    return _radialProfile.evaluate(r);
 }
 
 void local::TransformedProfileFunction::initTransform(TransformData* transformData) {
@@ -20,4 +29,12 @@ void local::TransformedProfileFunction::initTransform(TransformData* transformDa
 }
 
 void local::TransformedProfileFunction::doTransform() {
+    _transformData->tabulate(boost::bind(&local::TransformedProfileFunction::tabulator,this));
+}
+
+void local::TransformedProfileFunction::tabulator(double kx, double ky,
+local::TransformData::Complex& value) {
+    double kappa(_coordTransform.transformedWaveNumber(kx,ky));
+    value[0] = _radialProfile.radialIntegral(kappa);
+    value[1] = 0;
 }
--- imengine/TransformedProfileFunction.h
+++ imengine/TransformedProfileFunction.h
@@ -4,11 +4,14 @@
 #define IMENGINE_TRANSFORMED_PROFILE_FUNCTION
 
 #include "imengine/AbsPixelFunction.h"
+#include "imengine/TransformData.h"
 
 namespace imengine {
+    class AbsRadialProfile;
+    class AbsCoordTransform;
 	class TransformedProfileFunction : public AbsPixelFunction {
 	public:
-		TransformedProfileFunction();
+		TransformedProfileFunction(AbsRadialProfile const &profile, AbsCoordTransform &transform);
 		virtual ~TransformedProfileFunction();
 		// Returns the function value
         virtual double operator()(double x, double y) const = 0;
@@ -19,7 +22,11 @@ namespace imengine {
         // the transform object passed to initTransform(). The transform is defined as:
         // transform[m,n] = Sum[data[j,k] Exp[-2piI(j*m+k*n)/N],{j,0,N-1},{k,0,N-1}]
         virtual void doTransform() = 0;
+        
+        void tabulator(double kx, double ky, TransformData::Complex& value);
 	private:
+        AbsRadialProfile const &_radialProfile;
+        AbsCoordTransform const &_coordTransform;
         TransformData *_transformData;
 	}; // TransformedProfileFunction
 } // imengine
