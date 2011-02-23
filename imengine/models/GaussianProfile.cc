468d4f5cb1b0517df24074a174f01ffd7cec9606
David Kirkby <dkirkby@uci.edu>
2011-02-22 17:40:17 -0800

    implement gaussian profile

---
+++ imengine/models/GaussianProfile.cc
@@ -0,0 +1,27 @@
+// Created 22-Feb-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "imengine/models/GaussianProfile.h"
+
+#include <cassert>
+#include <cmath>
+
+namespace local = imengine::models;
+
+local::GaussianProfile::GaussianProfile(double sigma) {
+    assert(sigma > 0);
+    _twosigsq = 2*sigma*sigma;
+    _sigsqby2 = _twosigsq/4;
+    double root2pi(std::sqrt(8*std::atan(1.0)));
+    _norm = 1/(root2pi*sigma);
+    _integralNorm = sigma/root2pi;
+}
+
+local::GaussianProfile::~GaussianProfile() { }
+
+double local::GaussianProfile::operator()(double r) const {
+    return _norm*std::exp(-r*r/_twosigsq);
+}
+
+double local::GaussianProfile::radialIntegral(double kappa) const {
+    return _integralNorm*std::exp(-kappa*kappa*_sigsqby2);
+}
---
+++ imengine/models/GaussianProfile.h
@@ -0,0 +1,23 @@
+// Created 22-Feb-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef IMENGINE_MODELS_GAUSSIAN_PROFILE
+#define IMENGINE_MODELS_GAUSSIAN_PROFILE
+
+#include "imengine/AbsRadialProfile.h"
+
+namespace imengine {
+namespace models {
+	class GaussianProfile : public imengine::AbsRadialProfile {
+	public:
+		GaussianProfile(double sigma);
+		virtual ~GaussianProfile();
+		// Evaluates this profile at the specified radius.
+        virtual double operator()(double r) const;
+        // Evaluates this profile's real-valued radial integral for the specified kappa.
+        virtual double radialIntegral(double kappa) const;
+	private:
+        double _twosigsq, _sigsqby2, _norm, _integralNorm;
+	}; // GaussianProfile
+}} // imengine::models
+
+#endif // IMENGINE_MODELS_GAUSSIAN_PROFILE
