9d111589959784a3e1435eb09f4196ff64023f9e
David Kirkby <dkirkby@uci.edu>
2011-04-19 08:25:38 -0700

    Sersic model includes ellipticity

--- imengine/models/SersicDemo.cc
+++ imengine/models/SersicDemo.cc
@@ -9,18 +9,20 @@
 
 namespace local = imengine::models;
 
-local::SersicDemo::SersicDemo(double alpha, double index)
-: _alpha(alpha), _index(index)
+local::SersicDemo::SersicDemo(double alpha, double index, double e1, double e2)
+: _alpha(alpha), _index(index), _transform(e1,e2)
 {
     assertGreaterThan<double>("SersicDemo alpha",_alpha,0);
     assertGreaterThan<double>("SersicDemo index",_index,0);
+    _inverseIndex = 1/_index;
     double twopi(8*std::atan(1.0));
-    _norm = twopi*_index*std::pow(_alpha,-2*_index)*boost::math::tgamma(2*_index);
+    _norm = twopi*_index*std::pow(_alpha,-2*_index)*
+        boost::math::tgamma(2*_index)/_transform.determinant();
 }
 
 local::SersicDemo::~SersicDemo() { }
 
 double local::SersicDemo::operator()(double x, double y) const {
-    double rsq(x*x + y*y);
-    return std::exp(-_alpha*std::pow(rsq,1/(2*_index)))/_norm;
+    double r(_transform.transformedRadius(x,y));
+    return std::exp(-_alpha*std::pow(r,_inverseIndex))/_norm;
 }
--- imengine/models/SersicDemo.h
+++ imengine/models/SersicDemo.h
@@ -4,16 +4,18 @@
 #define IMENGINE_MODELS_SERSIC_DEMO
 
 #include "imengine/GenericPixelFunction.h"
+#include "imengine/EllipticityTransform.h"
 
 namespace imengine {
 namespace models {
 	class SersicDemo : public GenericPixelFunction {
 	public:
-		SersicDemo(double alpha, double index);
+		SersicDemo(double alpha, double index, double e1, double e2);
 		virtual ~SersicDemo();
 		virtual double operator()(double x, double y) const;
 	private:
-        double _alpha, _index, _norm;
+        double _alpha, _index, _norm, _inverseIndex;
+        EllipticityTransform _transform;
 	}; // SersicDemo
 }} // imengine::models
 
