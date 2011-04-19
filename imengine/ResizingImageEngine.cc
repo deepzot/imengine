eab6d18d05552936867d130e20e9f5b033e522b7
David Kirkby <dkirkby@uci.edu>
2011-04-19 13:09:02 -0700

    flesh out resizing engine logic

--- imengine/ResizingImageEngine.cc
+++ imengine/ResizingImageEngine.cc
@@ -1,9 +1,51 @@
 // Created 19-Apr-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
 
 #include "imengine/ResizingImageEngine.h"
+#include "imengine/ArrayImageWriter.h"
+#include "imengine/InvalidValue.h"
+
+#include <cassert>
 
 namespace local = imengine;
 
-local::ResizingImageEngine::ResizingImageEngine() { }
+local::ResizingImageEngine::ResizingImageEngine(AbsImageEnginePtr engine, int pixelMargin)
+: _generatedPixelData(new ArrayImageWriter()), _engine(engine), _pixelMargin(pixelMargin)
+{
+    assert(0 != _engine.get());
+}
 
 local::ResizingImageEngine::~ResizingImageEngine() { }
+
+void local::ResizingImageEngine::initialize(int pixelsPerSide, double pixelScale) {
+    AbsImageEngine::initialize(pixelsPerSide,pixelScale);
+    int genPixelsPerSide(pixelsPerSide + 2*_pixelMargin);
+    assertGreaterThanOrEqualTo<int>("ResizingImageEngine generated pixels per side",
+        genPixelsPerSide,1);
+    _engine->initialize(genPixelsPerSide, pixelScale);
+}
+
+double local::ResizingImageEngine::generate(AbsImageWriter &writer, double dx, double dy) {
+    // initialize the writer
+    int Nfinal(getPixelsPerSide());
+    writer.open(Nfinal,getPixelScale());
+    // use the engine to calculate oversampled data
+    double generatedSum(_engine->generate(*_generatedPixelData,dx,dy));
+    // resize to the final pixels
+    double finalSum(0);
+    int Ngen(_generatedPixelData->getSize());
+    for(int y = 0; y < Nfinal; ++y) {
+        int ygen(y + _pixelMargin);
+        for(int x = 0; x < Nfinal; ++x) {
+            int xgen(x + _pixelMargin);
+            double value(0);
+            // do we have generated data for this pixel?
+            if(ygen >= 0 && xgen >= 0 && ygen < Ngen && xgen < Ngen) {
+                value = _generatedPixelData->getValue(xgen,ygen);
+            }
+            writer.write(x,y,value);
+            finalSum += value;
+        }
+    }
+    writer.close();
+    return finalSum;
+}
--- imengine/ResizingImageEngine.h
+++ imengine/ResizingImageEngine.h
@@ -3,12 +3,38 @@
 #ifndef IMENGINE_RESIZING_IMAGE_ENGINE
 #define IMENGINE_RESIZING_IMAGE_ENGINE
 
+#include "imengine/AbsImageEngine.h"
+#include "imengine/ptr_types.h"
+
 namespace imengine {
-	class ResizingImageEngine {
+    class ArrayImageWriter;
+	class ResizingImageEngine : public AbsImageEngine {
 	public:
-		ResizingImageEngine();
+	    // Creates an engine that generates the image using a different pixel
+	    // size than requested, where the difference is specified by a pixel
+	    // margin size:
+	    //
+	    //   generatedPixelsPerSide = requestedPixelsPerSide + 2*pixelMargin
+	    //
+	    // Note that the pixel margin can be positive or negative. Positive values
+	    // are useful to reduce aliasing effects when the generated image has some
+	    // leakage outside the requested image size (that would otherwise be aliased
+	    // into the final image because of the assumed Fourier periodicity). Negative
+	    // values are useful to speed up generation when the image is centrally
+	    // concentrated and an outer pixel border can safely be forced to zero.
+		ResizingImageEngine(AbsImageEnginePtr engine, int pixelMargin);
 		virtual ~ResizingImageEngine();
+        // (Re)initializes the engine for the specified pixel grid size and scale.
+        // Throws an exception if the requested pixelsPerSide is too small for
+        // this engine's pixelMargin (i.e., pixelsPerSide + 2*pixelMargin <= 0).
+        virtual void initialize(int pixelsPerSide, double pixelScale = 1);
+		// Generates an image with the source function offset by (dx,dy) and returns
+		// the pixel sum
+        virtual double generate(AbsImageWriter &writer, double dx = 0, double dy = 0);
 	private:
+        boost::scoped_ptr<ArrayImageWriter> _generatedPixelData;
+        AbsImageEnginePtr _engine;
+        int _pixelMargin;
 	}; // ResizingImageEngine
 } // imengine
 
