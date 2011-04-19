56c0140855b49a71ead8b693f48b2e52f69e1e61
David Kirkby <dkirkby@uci.edu>
2011-04-19 08:28:29 -0700

    delegate tracks if params have ever been set via isInitialized() to avoid problem with all params initially set to zero fooling the cache test

--- imengine/ObserverDelegate.h
+++ imengine/ObserverDelegate.h
@@ -13,20 +13,27 @@ namespace imengine {
         ~ObserverDelegate();
         // Sets the observer we are delegated to update.
         void setObserver(ObserverPtr observer);
+        // Tests if we have been initialized by at least one call to setChanged().
+        bool isInitialized() const;
         // Registers that our result has changed.
         void setChanged();
 	private:
+        bool _initialized;
         ObserverPtr _observer;
 	}; // ObserverDelegate
 	
-	inline ObserverDelegate::ObserverDelegate() { }
+	inline ObserverDelegate::ObserverDelegate() : _initialized(false) { }
 	inline ObserverDelegate::~ObserverDelegate() { }
 
     inline void ObserverDelegate::setObserver(ObserverPtr observer) {
         _observer = observer;
     }
+    inline bool ObserverDelegate::isInitialized() const {
+        return _initialized;
+    }
     inline void ObserverDelegate::setChanged() {
         if(0 != _observer.get()) _observer->setChanged(true);
+        _initialized = true;
     }
 	
 } // imengine
