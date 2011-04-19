22d8e1fcb7ddf4bc21bd0a6872030defdb9aef3a
David Kirkby <dkirkby@uci.edu>
2011-04-19 11:01:23 -0700

    add skeleton new class

---
+++ imengine/ResizingImageEngine.cc
@@ -0,0 +1,9 @@
+// Created 19-Apr-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#include "imengine/ResizingImageEngine.h"
+
+namespace local = imengine;
+
+local::ResizingImageEngine::ResizingImageEngine() { }
+
+local::ResizingImageEngine::~ResizingImageEngine() { }
---
+++ imengine/ResizingImageEngine.h
@@ -0,0 +1,15 @@
+// Created 19-Apr-2011 by David Kirkby (University of California, Irvine) <dkirkby@uci.edu>
+
+#ifndef IMENGINE_RESIZING_IMAGE_ENGINE
+#define IMENGINE_RESIZING_IMAGE_ENGINE
+
+namespace imengine {
+	class ResizingImageEngine {
+	public:
+		ResizingImageEngine();
+		virtual ~ResizingImageEngine();
+	private:
+	}; // ResizingImageEngine
+} // imengine
+
+#endif // IMENGINE_RESIZING_IMAGE_ENGINE
